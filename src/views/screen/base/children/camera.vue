89e49aeafba4872b43f453f58c7f3181530cca52
noah.tan <noah.tan@greaconsulting.com>
2023-03-08 12:36:05 +0800

    update

--- src/views/screen/base/children/camera.vue
+++ src/views/screen/base/children/camera.vue
@@ -52,11 +52,7 @@ export default {
   },
   methods: {
     handleClose(done) {
-      this.$confirm('确认关闭？')
-        .then(_ => {
-          done();
-        })
-        .catch(_ => {});
+      done();
     }
   }
 }
