cdbf3e1805a1bc593d700141b7002ba6db8794d1
noah.tan <noah.tan@greaconsulting.com>
2023-03-08 12:37:56 +0800

    update

--- src/views/screen/event/children/eventList.vue
+++ src/views/screen/event/children/eventList.vue
@@ -45,12 +45,12 @@
         <el-table-column
           prop="date"
           label="日期"
-          width="180">
+          width="120">
         </el-table-column>
         <el-table-column
           prop="name"
           label="姓名"
-          width="180">
+          width="100">
         </el-table-column>
         <el-table-column
           prop="address"
